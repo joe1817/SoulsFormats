a2241df4c18fa117a432d095d2ba0c41e153f21c
Joe James <1817@duck.com>
2025-03-26 12:11:23 -0400

    Changed AES to reuse IV

--- SoulsFormats/Util/SFUtil.cs
+++ SoulsFormats/Util/SFUtil.cs
@@ -446,7 +446,8 @@ namespace SoulsFormats
                 aes.BlockSize = 128;
                 aes.Padding = PaddingMode.None;
                 aes.Key = key;
-                aes.GenerateIV();
+                //aes.GenerateIV();
+				aes.IV = new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f };
 
                 ICryptoTransform encryptor = aes.CreateEncryptor();
                 using (var decStream = new MemoryStream(decrypted))
